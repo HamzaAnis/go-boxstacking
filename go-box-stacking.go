cddb9fe0020400e989c5bd1cd364a615c82ab01b
HamzaAnis <hamzaanis9514@gmail.com>
2017-05-07 18:25:43 +0500

    Final

--- go-box-stacking.go
+++ go-box-stacking.go
@@ -1,8 +1,13 @@
 // package BoxStack
 package main
 
-import "github.com/fatih/color"
-import "math"
+import (
+	"fmt"
+	"math"
+	"sort"
+
+	"github.com/fatih/color"
+)
 
 type box struct {
 	l float64
@@ -10,16 +15,18 @@ type box struct {
 	h float64
 }
 
+//A variable to store the height
 var maxHeightR float64 = 0.0
 
+//A struct to sort
 type ByArea []box
 
 func (c ByArea) Len() int      { return len(c) }
 func (c ByArea) Swap(i, j int) { c[i], c[j] = c[j], c[i] }
 func (c ByArea) Less(i, j int) bool {
-	a := c[i].l * c[i].w
-	b := c[j].l * c[j].w
-	return a > b
+	a1 := c[i].area()
+	a2 := c[j].area()
+	return a1 < a2
 }
 
 func (b box) area() float64 {
@@ -30,6 +37,7 @@ func (b box) compare(compare box) float64 {
 	return b.area() - compare.area()
 }
 
+// BoxStack is a function to solve the Box stacking problem
 func BoxStack(b [][]float64) float64 {
 	if len(b) <= 0 {
 		color.Red("Not a valid length")
@@ -42,6 +50,12 @@ func BoxStack(b [][]float64) float64 {
 		boxes[i*3+2] = box{b[i][2], b[i][0], b[i][1]}
 	}
 	maxHeightR = 0
+	fmt.Println("Before Sorting")
+	display(boxes, len(b))
+	sort.Sort(ByArea(boxes))
+	fmt.Println("After Sorting")
+	display(boxes, len(b))
+
 	boxStackHelper(boxes, len(boxes)-1)
 	return maxHeightR
 }
@@ -61,28 +75,35 @@ func boxStackHelper(b []box, pos int) float64 {
 	return maxHeight
 }
 
-func findMax(a int, b int) int {
-	if a > b {
-		return a
-	} else {
-		return b
+func display(abox []box, a int) {
+	for i := 0; i < a; i++ {
+		fmt.Println(abox[i*3].l, abox[i*3].w, abox[i*3].h)
+		fmt.Println(abox[i*3+1].l, abox[i*3+1].w, abox[i*3+1].h)
+		fmt.Println(abox[i*3+2].l, abox[i*3+2].w, abox[i*3+2].h)
 	}
 }
-
 func main() {
 	var test1 = [][]float64{
 		{5, 5, 1},
 		{4, 5, 2},
 	}
 	var test2 = [][]float64{
+		{4, 7, 9},
+		{5, 8, 9},
+		{11, 20, 40},
+		{1, 2, 3},
+	}
+
+	var test3 = [][]float64{
 		{4, 6, 7},
 		{1, 2, 3},
 		{4, 5, 6},
 		{10, 12, 32},
 	}
-
 	result := BoxStack(test1)
 	color.Red("%v", result)
 	result = BoxStack(test2)
+	color.Red("%v", result)
+	result = BoxStack(test3)
 	color.Red("%v", result)
 }
